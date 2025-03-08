062b2a44b81a4eb7e98198124a5ed6ba1929cdf2
namangarg2002 <namangarg20021128@gmail.com>
2025-03-08 21:13:56 +0530

    Initial commit

---
+++ newsapp/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Spinner.css'
+const Spinner = () => {
+    return (
+        <div className='d-flex justify-content-center align-items-center'> 
+          <div className='spinner'></div> 
+        </div>
+    );
+};
+
+export default Spinner;
